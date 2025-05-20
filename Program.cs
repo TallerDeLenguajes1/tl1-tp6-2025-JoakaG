ef2aea96dab867c1283efa667924406f5b3e36b2
Joaquina GIrbau <nahuelcondori62t@gmail.com>
2025-05-20 13:24:19 -0300

    a

--- Program.cs
+++ Program.cs
@@ -1,31 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-int a;
-int b;
-a = 10;
-b = a;
-Console.WriteLine("Valor de a: " + a);
-Console.WriteLine("Valor de b: " + b);
 
-Console.Write("Ingresa un numero a invertir");
-int numero;
-if (int.TryParse(Console.ReadLine(), out numero) && numero > 0)
-{
-    Console.WriteLine("anana" + numero);
-    int numeroInvertido = 0;
-    while (numero != 0)
-    {
-        numeroInvertido = numeroInvertido * 10 + (numero % 10);
-        numero = numero / 10;
-    }
-    Console.WriteLine("Numero Invertido" + numeroInvertido);
-}
-else
-{
-    Console.WriteLine("Numero ingresado menor a 0 o NO ingresaste un número");
-
-}
 
 
 
