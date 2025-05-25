10698b2b6dd074b1b69cc7c3d889ccbdfd3ee786
JoakaG <girbaujoaquina2@gmail.com>
2025-05-24 22:36:39 -0300

    Calculadora V2 funcionando

--- Program.cs
+++ Program.cs
@@ -6,81 +6,145 @@ Console.WriteLine("Hello, World!");
 
 int opcion = -1;
 do
-{.
-    Console.WriteLine("1. Obtener: \n ●El valor absoluto de un número \n● El cuadrado \n ● La raíz cuadrada \n ● El seno \n ● El Coseno \n ● La parte entera \n 0. Salir");
-    if (int.TryParse(Console.ReadLine(), out opcion) && opcion != 0 && opcion < 2)
+{
+    Console.WriteLine("\n---Seleccione una opción---");
+    Console.WriteLine("1. Valor absoluto de un número");
+    Console.WriteLine("2. Cuadrado de un número");
+    Console.WriteLine("3. Raíz cuadrada de un número");
+    Console.WriteLine("4. Seno de un número");
+    Console.WriteLine("5. Coseno de un número");
+    Console.WriteLine("6. Parte entera de un número");
+    Console.WriteLine("7. Mínimo entre dos números");
+    Console.WriteLine("8. Máximo entre dos números");
+    Console.WriteLine("0. Salir");
+    if (int.TryParse(Console.ReadLine(), out opcion) && opcion != 0 && opcion < 9)
     {
-        float a = FuncionIngresarNumeros();
-        realizarOperaciones(a);
+        realizarOperaciones(opcion);
     }
     else
     {
         opcion = 0;
     }
-} while (opcion > 0 && opcion < 2);
+} while (opcion > 0 && opcion <= 8);
 Console.WriteLine("Finalizando programa...");
 
-float FuncionIngresarNumeros()
+float FuncionIngresarNumero()
 {
-    Console.WriteLine("Ingresa el primer número entero");
-    string ingreso1 = Console.ReadLine();
-    if (float.TryParse(ingreso1, out float numero1))
+    Console.WriteLine("Ingresa número (utilizar , como separador decimal en caso de necesitarlo)");
+    string ingreso = Console.ReadLine();
+    if (float.TryParse(ingreso, out float numero))
     {
-        return numero1;
+        return numero;
     }
     else
     {
-        return 1.45730625279;
+        return 1.45730625279f;
     }
 }
 
 
-void realizarOperaciones(float a)
+
+void realizarOperaciones(int opcion)
 {
-    FuncionAbsoluto(a);
-    FuncionCuadrado(a);
-    FuncionRaizCuadrada(a);
-    FuncionSeno(a);
-    FuncionCoseno(a);
-    FuncionParteEntera(a);
+    float a;
+    float b = 0f;
+    if (opcion < 7)
+    {
+        a = FuncionIngresarNumero();
+    }
+    else
+    {
+        a = FuncionIngresarNumero();
+        b = FuncionIngresarNumero();
+    }
+    if (a == 1.45730625279 || b == 1.45730625279)
+    {
+        Console.WriteLine("ERROR no se ingresó un número válido");
+        return;
+    }
+    switch (opcion)
+    {
+        case 1:
+            FuncionAbsoluto(a);
+            break;
+        case 2:
+            FuncionCuadrado(a);
+            break;
+        case 3:
+            FuncionRaizCuadrada(a);
+            break;
+        case 4:
+            FuncionSeno(a);
+            break;
+
+        case 5:
+            FuncionCoseno(a);
+            break;
+        case 6:
+            FuncionParteEntera(a);
+            break;
+        case 7:
+            
+            FuncionMin(a, b);
+            break;
+        case 8:
+            FuncionMax(a, b);
+            break;
+        default:
+            break;
+    }
 }
 
 
 void FuncionAbsoluto(float a)
 {
-    Console.WriteLine("\n\nResultado de " + a + ": " + Math.Abs(a));
+    Console.WriteLine("\n\n|"+a+"|= "+ Math.Abs(a));
 }
 
 void FuncionCuadrado(float a)
 {
-    Console.WriteLine("\n\nResultado de " + a + ": " + (a * a));
+    Console.WriteLine("\n\n"+a +"^2"+ "= " + (a * a));
 }
 void FuncionRaizCuadrada(float a)
 {
-    Console.WriteLine("\n\nResultado de " + a + ": " + Math.Sqrt(a));
+    Console.WriteLine("\n\n√" + a + "=" + Math.Sqrt(a));
 }
 
 void FuncionParteEntera(float a)
 {
-    Console.WriteLine("\n\nResultado de " + a + ": " + Math.(a));
+    Console.WriteLine("\n\n[" + a + "]= " + Math.Floor(a));
 }
 
 void FuncionSeno(float a)
 {
-    Console.WriteLine("\n\nResultado de " + a + ": " + Math.Sin(a));
+    Console.WriteLine("\n\nsen " + a + "= " + Math.Sin(a));
 }
 
 void FuncionCoseno(float a)
 {
-    Console.WriteLine("\n\nResultado de " + a + ": " + Math.Cos(a));
+    Console.WriteLine("\n\ncos " + a + "= " + Math.Cos(a));
 }
 
 void FuncionMax(float a, float b)
 {
-    Console.WriteLine("\n\nResultado de " + a + ": " + Math.Abs(a));
+    if (a >= b)
+    {
+        Console.WriteLine("\n\n Número mayor: " + a);
+    }
+    else
+    {
+        Console.WriteLine("\n\n Número mayor: " + b);
+    }
 }
 
 void FuncionMin(float a, float b)
 {
-    Console.WriteLine("\n\nResultado de " + a + ": " + Math.Abs(a));
+    if (a < b)
+    {
+        Console.WriteLine("\n\n Número menor: " + a);
+    }
+    else
+    {
+        Console.WriteLine("\n\n Número menor: " + b);
+    }
 }
